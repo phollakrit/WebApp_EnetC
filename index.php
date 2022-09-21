8f5c08642c72d4a0d0739facc1b06d8c967826c6
pat123 <phollakrit@hotmail.com>
2022-09-21 11:13:39 +0700

    work5

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +9,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Webboard Kakkak</title>
 </head>
+<?php
+    if(!isset($_SESSION['id'])) {
+?>
 <body>
     <h1 style="text-align: center;">Webboard KakKak</h1>
     <hr>
@@ -15,7 +21,7 @@
         <option value="general">เรื่องทั่วไป</option>
         <option value="study">เรื่องเรียน</option>
     </select>
-    <a href="login.html" style="float: right;">เข้าสู่ระบบ</a>
+    <a href="login.php" style="float: right;">เข้าสู่ระบบ</a>
     <br>
     <ul>
         <?php 
@@ -27,4 +33,41 @@
 
     </ul>
 </body>
+<?php 
+    }else{
+?>
+<body>
+    <h1 style="text-align: center;">Webboard KakKak</h1>
+    <hr>
+    หมวดหมู่: 
+    <select name="category">
+        <option value="all">--ทั้งหมด--</option>
+        <option value="general">เรื่องทั่วไป</option>
+        <option value="study">เรื่องเรียน</option>
+    </select>
+
+    <div style="float:right">
+        <?php echo "ผู้ใช้งานระบบ : $_SESSION[username]";?>&nbsp;&nbsp;
+        <a href="logout.php">ออกจากระบบ</a>
+    </div>
+    <br>
+    <a href="newpost.php">สร้างกระทู้ใหม่</a>
+    <br>
+    <ul>
+        <?php 
+            for($i=1;$i<=10;$i++){
+                echo "<li>";
+                echo "<a href=post.php?id=$i>กระทู้ที่ $i</a>";
+                if($_SESSION['role']=='a'){
+                    echo "&nbsp;&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+                }
+                echo "</li>";
+            }        
+        ?>
+
+    </ul>
+</body>
+<?php
+    }
+?>
 </html>
--- login.php
+++ login.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if(isset($_SESSION['id'])){
+        header("location:index.php");
+        die();
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,6 +26,6 @@
         </table>
     </form>
     <br>
-    <div align="center">ถ้ายังไม่ได้เป็นสมาชิก <a href="register.html">กรุณาสมัครสมาชิก</a></div>    
+    <div align="center">ถ้ายังไม่ได้เป็นสมาชิก <a href="register.php">กรุณาสมัครสมาชิก</a></div>    
 </body>
 </html>
