d0e291decaa2c67c7ff10d85bcc288284af111ab
pat123 <phollakrit@hotmail.com>
2022-10-26 12:13:42 +0700

    work7

--- index.php
+++ index.php
@@ -58,34 +58,43 @@
     <div class="container">
     <h1 style="text-align: center;">Webboard KakKak</h1>
     <?php include "nav.php"; ?>
-    <hr>
-    หมวดหมู่: 
-    <select name="category">
-        <option value="all">--ทั้งหมด--</option>
-        <option value="general">เรื่องทั่วไป</option>
-        <option value="study">เรื่องเรียน</option>
-    </select>
-
-    <div style="float:right">
-        <?php echo "ผู้ใช้งานระบบ : $_SESSION[username]";?>&nbsp;&nbsp;
-        <a href="logout.php">ออกจากระบบ</a>
+    <br>
+    <div class="d-flex justify-content-between">
+        <div>
+            <label>หมวดหมู่</label>
+            <span class="dropdown">
+                <button class="btn btn-light dropdown-toggle btn-sm"
+                 type="button" id="button2" data-bs-toggle="dropdown"
+                  aria-expanded="false">--ทั้งหมด--</button>
+                <ul class="dropdown-menu" aria-labelledby="button2">
+                    <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+                    <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+                </ul>
+            </span>
+        </div>
+        <div>
+            <a href="newpost.php" class="btn btn-success btn-sm">
+            <i class="bi bi-plus"></i> สร้างกระทู้ใหม่</a>
+        </div>
     </div>
     <br>
-    <a href="newpost.php">สร้างกระทู้ใหม่</a>
-    <br>
-    <ul>
+    <table class="table table-striped">
+   
         <?php 
             for($i=1;$i<=10;$i++){
-                echo "<li>";
-                echo "<a href=post.php?id=$i>กระทู้ที่ $i</a>";
+                echo "<tr><td><a href=post.php?id=$i style=text-decoration:none>กระทู้ที่ $i</a></td>";
                 if($_SESSION['role']=='a'){
-                    echo "&nbsp;&nbsp;&nbsp;<a href=delete.php?id=$i>ลบ</a>";
+                    echo "<td><a href=delete.php?id=$i class='btn btn-danger btn-sm'>
+                    <i class='bi bi-trash'></i></a></td>";
                 }
-                echo "</li>";
-            }        
+                echo "</tr>";
+            }
+            
         ?>
 
-    </ul>
+    
+    </table>
     </div>
 </body>
 <?php
